c07f1c1d61aafca8ff02c8f94c20197175957c12
Duarte Almeida <duartemalmeida@gmail.com>
2021-03-13 11:43:42 +0000

    add the functions to the data form

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from time import sleep
 from notification_manager import NotificationManager
 from search_manager import ApartmentHuntBot
+from data_form import DataForm
 
 
 bot = ApartmentHuntBot()
 notification = NotificationManager()
+data = DataForm()
 
 bot.search_input()
 sleep(5)
 
 bot.get_search_results()
+sleep(5)
+
+data.get_data_fields()
+sleep(5)
+data.send_data()
 
 bot.driver.quit()
